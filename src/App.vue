bb21cf7709c84e3be768363711accb8e9d4cb392
OceanPresent <oceanpresent@163.com>
2022-05-05 23:47:59 +0800

    chore:init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <main>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div>
+      <HelloWorld msg="You did it!" />
+    </div>
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+const count = ref(0)
+function increment() {
+  count.value++
+}
+
+function decrease() {
+  count.value--
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>+
+      <a target="_blank" href="https://vueuse.org/">VueUse</a>.
+    </h3>
+    <div>
+      <div>
+        Count:{{ count }}
+      </div>
+      <button @click="increment">
+        add count
+      </button>
+      <button @click="decrease">
+        decrease count
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: rgb(30, 218, 30);
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+.greetings {
+  text-align: center;
+}
+</style>

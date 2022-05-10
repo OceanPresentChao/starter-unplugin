53126f6a9988aa2d35424f52b2323932b4703b2a
OceanPresent <oceanpresent@163.com>
2022-05-10 11:14:33 +0800

    🔧 feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  content: [],
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },

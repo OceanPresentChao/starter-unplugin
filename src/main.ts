92614883bf16e0c90f881e03564a45e450003f20
OceanPresent <oceanpresent@163.com>
2022-05-11 20:07:53 +0800

    :sparkles: feat: iconify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
-createApp(App).mount('#app')
+import { Icon } from '@iconify/vue';
+const app = createApp(App)
+app.component("Icon", Icon)
+app.mount('#app')

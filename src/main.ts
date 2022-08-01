9b6237abe69ad8a30c5e05961e366ac408e83c6a
OceanPresent <oceanpresent@163.com>
2022-08-01 21:34:43 +0800

    :sparkles: feat: update pnpm

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './index.css'
 import { Icon } from '@iconify/vue'
 import App from './App.vue'
-import { i18n } from '@/locales'
+import { i18n } from './locales'
 const app = createApp(App)
 app.component('Icon', Icon)
 app.use(i18n)
